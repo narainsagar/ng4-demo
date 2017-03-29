97c74f45974d62e425e835218eb3b1fb02bcdee7
Narain Sagar <narainmenghwar@gmail.com>
2017-03-29 08:43:10 +0500

    minor adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,44 +9,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  userList: any[];
-
-  counter$: Observable<number>;
-  even$: Observable<boolean>;
-  people$: Observable<any>;
-  person$: Observable<any>;
-  data: any;
+  title = 'angular v4 demo!';
 
   constructor(private router: Router) {
-      this.counter$ = Observable.interval(1000);
-
-      this.even$ = this.counter$.map(number => number % 2 === 0);
-
-      this.people$ = Observable.of([
-              {name: 'Joe'},
-              {name: 'Bob'},
-              {name: 'Susy'}
-          ]);
-
-      this.person$ = Observable.of({
-              name: 'Sally',
-              age: 30
-          })
-          .delay(3000);
-
-      this.data = this.getData();
   }
 
   checkCurrentRoute(routeStr: string) {
       return this.router.url.split('/').indexOf(routeStr) !== -1;
-  }
-
-  getData() {
-      return new Promise(resolve => {
-          setTimeout(() => {
-              resolve('RECEIVED DATA!');
-          }, 5000);
-      });
   }
 }
